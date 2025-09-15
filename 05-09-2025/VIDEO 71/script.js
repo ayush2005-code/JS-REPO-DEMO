30336d3bb18ccb744b6776b9766e4c5ebf4ed79d
ayush2005-code <ayush772005@gmail.com>
2025-09-15 19:42:33 +0530

    learned about attributes,hidden property,design mode,changing innerhtml

--- 05-09-2025/VIDEO 71/script.js
+++ 05-09-2025/VIDEO 71/script.js
@@ -23,3 +23,60 @@ console.log("when node Name used it returns the name of the node only");
 console.log("A node can include various tags(elements) text attribute eventhough everything in DOM is treated as node even comment node is also there ");
 console.log(element.nodeName);
 
+
+//5)hidden property
+     // element.hidden="true"; hides the div container and its content with it 
+
+
+
+
+
+
+     //similarly we can change inner html of an element so we can change it using 
+
+     let node = document.querySelector(".box");
+     node.innerHTML="the content is changed";
+
+     //thus the content gets changed in first box
+
+
+//(*)  ATTRIBUTE-METHODS
+  // to check whether the following element has require attribute we use the:- 
+  //(1)hasAttribute(name)
+  node.hasAttribute("style");
+  //returns false 
+
+  //if div.box style:display:"flex";
+  // then we use node.hasAttribute("style");
+  //it will return true; as it is present 
+
+  //(2)getAttribute(name)
+  //tells us about the attribute and its content in it
+  node.getAttribute("style");
+  //returns the content inside it for eg:-
+  //if div.box style:display:"flex"; then 
+  //node.getAttribute("style"); returns 
+  // display:"flex";
+
+  //(3)setAttribute(name,value)
+  //if we dont like any attribute content we can replace it 
+  node.setAttribute("style","display:inline");
+  //dynamically the style attribute display property gets replaced by inline 
+  //it works as check for the first box having style attribute and replace
+  //its display:flex property by inline property;
+
+  //(4)removeAttribute(name)
+  //removes the attribute from the given element 
+
+  //(5)element.attributes
+  //gets us collection of attributes present on a element 
+
+
+
+  //(*)data --> attribute 
+  //we can create our own attribute by using data-nameofattribute
+  //for which we need to use keyword "data-"
+  //this gets stored in dataset and can be accessed 
+  //using element.dataset all these attributes are stored in key value pairs like objects 
+  node.dataset
+  //returns 
